813e9a79145c71fba8569ef9606c40fed4cd434e
Paul Plischke <paul.plischke@students.hs-mainz.de>
2026-06-15 18:30:35 +0200

    included parking spaces in the data

--- parking-service/initdb/backup.sql
+++ parking-service/initdb/backup.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f4696ef59795a8f99a496fc5285d359c8162a0aeb0313b2f512df1c19302a2c
-size 188013655
+oid sha256:5cf911ee0b4b162c3906fc8ad69ad00f3b21dc0f2a01ccf012abe10df1926eba
+size 423068380
