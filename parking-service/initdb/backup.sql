ca79b339f2e58afdfe2418c712c0533cbd3c3dbb
GoldenKoopa <100142836+GoldenKoopa@users.noreply.github.com>
2026-06-15 22:02:28 +0200

    fix encoding of data

--- parking-service/initdb/backup.sql
+++ parking-service/initdb/backup.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9b2e8491cf21daedfb321f8b5dc08127f56c94bc4e957f7ebbea5434c88b09b
-size 423068068
+oid sha256:4f44a970046ceec878ee7ea595d81601bc3be208703f505cce8e669bdd8f11e4
+size 210998359
